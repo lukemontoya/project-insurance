40968de76f9dad55153574da92567884318dd2c4
Luke Montoya <plane.luke@gmail.com>
2018-06-05 12:56:27 -0700

    changed link on profile again

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -2,7 +2,16 @@ const knex = require("../db/knex.js");
 
 
 module.exports = {
-    index: function (req, res) {
-        res.render('appointments')
-    },
+    index: function(req, res) {
+    knex('agents')
+      .where('id', req.params.id)
+        .then((agents)=>{
+          knex('appointments')
+            .where('agent_id', req.params.agent_id)
+            
+            .then((appointments)=>{
+              res.render('confimation', {agents:agents[0], appointments:appointments});
+        });
+      })
+  }
 }
