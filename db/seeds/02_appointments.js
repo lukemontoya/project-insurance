6b438adaf598243e3d10d78ae4bb2f3723a5ad32
Luke Montoya <plane.luke@gmail.com>
2018-06-06 09:24:01 -0700

    seedfix

--- db/seeds/02_appointments.js
+++ db/seeds/02_appointments.js
@@ -6,7 +6,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('appointments').insert([
         {client_name: 'Crash Carwrecks', client_email: 'crash@boom.com', date: '6-13-2018', client_comment: 'I need car insurance like whoa', agent_id: 1},
-        {client_name: 'Grim Reaper', client_email: 'grim@dead.com', date: '6-8-2018', client_comment: 'Can I take out policies on other lives?', status: 'confirmed' agent_id: 1},
+        {client_name: 'Grim Reaper', client_email: 'grim@dead.com', date: '6-8-2018', client_comment: 'Can I take out policies on other lives?', status: 'confirmed', agent_id: 1},
         {client_name: 'Carlos Enfuego', client_email: 'burn@fire.com', date: '6-4-2018', client_comment: 'I need a home policy with a good fire clause because the urges are coming back',  status: 'completed', agent_id: 1}
       ]);
     });
