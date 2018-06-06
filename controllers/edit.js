c4d6ae7d3dbfc6d5cc831abbe1ae93adbcc8000a
Chris Hervois <chris.hervois@gmail.com>
2018-06-05 20:10:11 -0700

    Photo upload fully functional

--- controllers/edit.js
+++ controllers/edit.js
@@ -1,5 +1,14 @@
+const express = require("express");
+const path = require("path");
 const knex = require("../db/knex.js");
 const hasher = require("../config/hasher");
+const app = express();
+const AWS = require('aws-sdk');
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
+AWS.config.loadFromPath('./config.json');
+const s3Bucket = new AWS.S3({ params: { Bucket: "q2insuranceproject" } });
+const baseAWSURL = "https://s3.amazonaws.com/q2insuranceproject/"
 
 module.exports = {
     index: function (req, res) {
@@ -9,6 +18,20 @@ module.exports = {
             })
     },
     update: function (req, res) {
+        // console.log(req.body)
+        // console.log(req.files.upload)
+        let uploadData = {
+            Key: req.body.email,
+            Body: req.files.upload.data,
+            ContentType: req.files.upload.mimetype,
+            ACL: 'public-read'
+        }
+        s3Bucket.putObject(uploadData, function (err, data) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+        })
         if (req.body.password && (req.body.password === req.body.confirm)) {
             
             hasher.hash(req.body).then(user => {
@@ -17,7 +40,7 @@ module.exports = {
                     agent_name: user.name,
                     agent_email: user.email,
                     bio: user.bio,
-                    IMG_url: user.img_url,
+                    IMG_url: baseAWSURL + uploadData.Key,
                     location: user.location,
                     home: user.home ? user.home : false,
                     car: user.car ? user.car : false,
